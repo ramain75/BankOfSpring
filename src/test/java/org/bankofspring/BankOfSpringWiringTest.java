3dd8fcd33c222af7a6ac1f3c4dc3b5624df3323f
Thierry Roisin <Thierry.Roisin@datacash.com>
2015-04-28 13:58:31 +0100

    add test

---
+++ src/test/java/org/bankofspring/BankOfSpringWiringTest.java
@@ -0,0 +1,15 @@
+package org.bankofspring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankOfSpringWiringTest {
+
+	@Test
+	public void test() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("BankOfSpringApp.xml");
+		
+	}
+
+}
