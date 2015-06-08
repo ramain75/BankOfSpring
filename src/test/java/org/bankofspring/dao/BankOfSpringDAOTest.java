7a50dd72228025970eb234cc859a2c92720eaa49
malcolmmurray <malcolm@greenspacelive.com>
2015-06-08 23:03:06 +0100

    more work

--- src/test/java/org/bankofspring/dao/BankOfSpringDAOTest.java
+++ src/test/java/org/bankofspring/dao/BankOfSpringDAOTest.java
@@ -1,7 +1,9 @@
 package org.bankofspring.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import org.bankofspring.model.Account;
 import org.junit.*;
 
 import static org.junit.Assert.*;
@@ -19,7 +21,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class BankOfSpringDAOTest {
 
 	@Autowired
-	AccountsDaoImpl bankOfSpringDao;
+	AccountsDaoImpl accountsDao;
 	
 	/**
 	 * Basic test to make sure we have a DB connection
@@ -27,7 +29,19 @@ public class BankOfSpringDAOTest {
 	 */
 	@Test
 	public void testConnectionIsValid() throws SQLException{
-		assertTrue(bankOfSpringDao.getDataSource().getConnection().isValid(1));
+		assertTrue(accountsDao.getDataSource().getConnection().isValid(1));
+	}
+	
+	@Test
+	public void testGetAccounts(){
+		
+		List<Account> accounts = accountsDao.getAccounts();
+		
+		assertEquals("We have accounts in the db", 4, accounts.size());
+		assertEquals("First row should be account 1", "account1" ,accounts.get(0).getAccountNumber());
+		assertEquals("First row should be account 2", "account1" ,accounts.get(0).getAccountNumber());
+		assertEquals("First row should be account 3", "account1" ,accounts.get(0).getAccountNumber());
+		assertEquals("First row should be account 4", "account1" ,accounts.get(0).getAccountNumber());	
 	}
 	
 }
