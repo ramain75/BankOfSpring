a1cbe97d3df34cd2024b09bb5d40bc86c2c71c51
txr12 <txr12@nowhere.com>
2015-06-23 08:00:48 +0100

    first changes for hibernate nowhere near finished

---
+++ src/test/java/org/bankofspring/dao/hibernate/CustomerDAOHibernateTest.java
@@ -0,0 +1,33 @@
+package org.bankofspring.dao.hibernate;
+
+import static org.junit.Assert.assertEquals;
+
+import org.bankofspring.dao.CustomerDAO;
+import org.bankofspring.model.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:BankOfSpring-ds-test.xml",
+		"classpath:BankOfSpring.xml" })
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
+public class CustomerDAOHibernateTest {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+
+	@Test
+	public void testGetCustomerById() throws Exception {
+		Customer customer = customerDAO.getCustomerById(1);
+		assertEquals((Integer) 1, customer.getId());
+		assertEquals("customer one", customer.getName());
+		assertEquals("test", customer.getPassword());
+		assertEquals("user1", customer.getUsername());
+	}
+
+}
