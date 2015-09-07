cf0a3db2303d070036825f5ce3139fa092980f23
unknown <e036324@.corp.mastercard.org>
2015-09-07 18:35:29 +0100

    add customer works

--- src/main/java/org/bankofspring/controllers/CustomerController.java
+++ src/main/java/org/bankofspring/controllers/CustomerController.java
@@ -5,22 +5,32 @@ import java.util.Map;
 
 import org.bankofspring.dao.CustomerDAO;
 import org.bankofspring.model.Customer;
+import org.bankofspring.web.CustomerForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
 	@Autowired
 	CustomerDAO customerDao;
-	@RequestMapping ("/")
+	@Autowired
+	CustomerForm form;
+	@RequestMapping (value="/",method=RequestMethod.GET)
 	public String showCustomerList(Map<String,Object> model) {
 		List<Customer> customers = customerDao.getCustomers();
 		model.put("customers",customers);
 		return "customerslist";
 	}
-	@RequestMapping ("/new")
+	@RequestMapping (value="/new", method=RequestMethod.GET)
 	public String newCustomer(Map<String,Object> model) {
+		model.put("customerform",form);
 		return "newcustomer";
+	}
+	@RequestMapping (value="/addcustomer", method=RequestMethod.POST)
+	public String addCustomer(CustomerForm form) {
+		Customer customer = customerDao.addCustomer(form.getName(), form.getEmail(), form.getDescription());
+		return "redirect:/customers/";
 	}
 }
---
+++ src/main/java/org/bankofspring/web/CustomerForm.java
@@ -0,0 +1,29 @@
+package org.bankofspring.web;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerForm {
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String email;
+	private String description;
+	
+}
