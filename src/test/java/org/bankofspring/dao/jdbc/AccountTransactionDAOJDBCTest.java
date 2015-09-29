960a06db89c5e159137a06db2ae46b65041aa38c
Alex Panayotopoulos <alex.panayotopoulos@datacash.com>
2015-09-29 16:13:46 +0100

    Get Jetty working, fix tests, fix resources

--- src/test/java/org/bankofspring/dao/jdbc/AccountTransactionDAOJDBCTest.java
+++ src/test/java/org/bankofspring/dao/jdbc/AccountTransactionDAOJDBCTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.jdbc.SimpleJdbcTestUtils;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:BankOfSpring-ds-test.xml", "classpath:BankOfSpring.xml"})
+@ContextConfiguration({"classpath:BankOfSpring-ds.xml", "classpath:BankOfSpring.xml"})
 @DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 public class AccountTransactionDAOJDBCTest {
 
